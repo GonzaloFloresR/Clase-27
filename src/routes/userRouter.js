b61e7f1d33fdde1f08b1fa82b0093176976490fd
gonzaloflores <gonzalof@hotmail.com>
2024-07-28 18:05:52 -0400

    url - Premium

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -8,7 +8,7 @@ const router = Router();
 
 router.get("/", (req, res)=>{
     res.setHeader("Content-Type","application/json");
-            return res.status(400).json({"status":`Error, Debe ingresar un Id de usuario válido`});
+    return res.status(400).json({"status":`Error, Debe ingresar un Id de usuario válido`});
 });
 
 router.get("/:uid",async(req, res)=>{
@@ -24,7 +24,7 @@ router.get("/:uid",async(req, res)=>{
             res.setHeader("Content-Type","application/json");
             return res.status(400).json({"status":`Error, No existe usuario con el ID ${uid}`});
         }
-        if(usuario.rol == "user"){
+        if(usuario.rol === "user"){
             usuario.rol = "premium";
             actualizar = await userDAO.updateUsuario(uid,usuario);
             if(!actualizar){
@@ -32,16 +32,16 @@ router.get("/:uid",async(req, res)=>{
                 return res.status(400).json({"status":`Error, No se pudo actualizar el usuario`});
             }
             res.setHeader("Content-Type","application/json");
-            return res.status(200).json({"status":`Rol del usuario ${usuario.first_name} ${usuario.last_name} actualizado`});
-        } else if(usuario.rol == "premium"){
-            usuario.rol = "premium";
+            return res.status(200).json({"status":`Rol del usuario ${usuario.first_name} ${usuario.last_name} actualizado por ${usuario.rol}`});
+        } else if(usuario.rol === "premium"){
+            usuario.rol = "user";
             actualizar = await userDAO.updateUsuario(uid,usuario);
             if(!actualizar){
                 res.setHeader("Content-Type","application/json");
                 return res.status(400).json({"status":`Error, No se pudo actualizar el usuario`});
             }
             res.setHeader("Content-Type","application/json");
-            return res.status(200).json({"status":`usuario actualizado ${usuario.first_name} ${usuario.last_name}`});
+            return res.status(200).json({"status":`Rol del usuario ${usuario.first_name} ${usuario.last_name} actualizado por ${usuario.rol}`});
         } else {
             let rol = usuario.rol;
             res.setHeader("Content-Type","application/json");
